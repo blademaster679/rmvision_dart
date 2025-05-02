1308716c1f04e9619f9de23ee4ef1883eb7fe3dc
blademaster679 <2816020017@qq.com>
2025-05-02 23:50:29 +0800

    第一次成功识别并与电控通讯

--- src/rm_serial_driver/include/packet.hpp
+++ src/rm_serial_driver/include/packet.hpp
@@ -26,7 +26,7 @@ struct SendPacket
   uint8_t header = 0xA5;
 //   uint8_t tracking : 1;
 //   uint8_t iffire : 1;
-//   uint8_t id : 3;          // 0-outpost 6-guard 7-base
+//   uint8_t id : 3;    0-outpost 6-guard 7-base
 //   uint8_t reserved : 3;
   
   float distance;
